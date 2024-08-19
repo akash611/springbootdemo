e83ba305e7f5bd32513083886c125978d1081a3a
akash.majida <akash.majida@gmail.com>
2024-08-19 13:36:35 +0000

    developing the environment for the project

---
+++ .idx/dev.nix
@@ -0,0 +1,41 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.zulu17
+    pkgs.maven
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "vscjava.vscode-java-pack"
+      "rangav.vscode-thunder-client"
+      "redhat.fabric8-analytics"
+      "redhat.java"
+      "vscjava.vscode-java-debug"
+      "vscjava.vscode-java-dependency"
+      "vscjava.vscode-java-test"
+      "vscjava.vscode-maven"
+      "Pivotal.vscode-boot-dev-pack"
+      "vmware.vscode-spring-boot"
+      "vscjava.vscode-spring-boot-dashboard"
+      "vscjava.vscode-spring-initializr"
+      
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install = "mvn clean install";
+      };
+      # Runs when a workspace is (re)started
+      onStart = {
+        run-server = "PORT=3000 mvn spring-boot:run";
+      };
+    };
+  };
+}
